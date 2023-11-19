98de1a331cc678adb5cf307a24d9cfd27181ff69
Zek <zekai@fuzz.land>
2023-11-19 08:57:01 +0800

    Support setup file.

--- cli.js
+++ cli.js
@@ -34,66 +34,76 @@ function visualize(results) {
     console.log(table(data))
 }
 
-async function build_with_autodetect(project, projectType, compiler_version, daemon, autoStart) {
-    console.log("Starting anvil...");
-    const anvil = exec('anvil');
-
-    process.on('SIGINT', () => {
-        anvil.kill();
-    });
+async function build_with_autodetect(project, projectType, compiler_version, daemon, autoStart, setupFile) {
 
     if (!projectType) {
         projectType = await auto_detect(project);
     }
+
     let results = await build(projectType, project, compiler_version);
 
     if (!Array.isArray(results)) {
         results = [results];
     }
 
-    let offchainConfig = results.reduce((acc, item) => {
-        const deepClonedItem = JSON.parse(JSON.stringify(item.abi));
-        return { ...acc, ...deepClonedItem };
-    }, {});
+    let anvil = null;
 
-    for (const fileName in offchainConfig) {
-        for (const contractName in offchainConfig[fileName]) {
-            offchainConfig[fileName][contractName] = {
-                "address": randomAddress(),
-                "constructor_args": "0x"
+    if (!setupFile) {
+        console.log("Starting anvil...");
+        anvil = exec('anvil');
+
+        process.on('SIGINT', () => {
+            anvil.kill();
+        });
+
+        let offchainConfig = results.reduce((acc, item) => {
+            const deepClonedItem = JSON.parse(JSON.stringify(item.abi));
+            return { ...acc, ...deepClonedItem };
+        }, {});
+
+        for (const fileName in offchainConfig) {
+            for (const contractName in offchainConfig[fileName]) {
+                offchainConfig[fileName][contractName] = {
+                    "address": randomAddress(),
+                    "constructor_args": "0x"
+                }
             }
         }
-    }
 
-    fs.writeFileSync("offchain_config.json", JSON.stringify(offchainConfig, null, 4));
+        fs.writeFileSync("offchain_config.json", JSON.stringify(offchainConfig, null, 4));
 
-    console.log("Offchain config written to offchain_config.json, please edit it to specify the addresses of the contracts and press enter to continue");
+        console.log("Offchain config written to offchain_config.json, please edit it to specify the addresses of the contracts and press enter to continue");
 
-    await new Promise((resolve) => {
-        process.stdin.once('data', (chunk) => {
-            resolve();
+        await new Promise((resolve) => {
+            process.stdin.once('data', (chunk) => {
+                resolve();
+            });
         });
-    });
 
-    let artifacts = await deploy(results, offchainConfig);
+        let artifacts = await deploy(results, offchainConfig);
 
-    for (const artifact of artifacts) {
-        for (const fileName in artifact["address"]) {
-            for (const contractName in artifact["address"][fileName]) {
-                if (offchainConfig.hasOwnProperty(fileName) && offchainConfig[fileName].hasOwnProperty(contractName) && artifact["address"][fileName][contractName]) {
-                    offchainConfig[fileName][contractName]["address"] = artifact["address"][fileName][contractName];
+        for (const artifact of artifacts) {
+            for (const fileName in artifact["address"]) {
+                for (const contractName in artifact["address"][fileName]) {
+                    if (offchainConfig.hasOwnProperty(fileName) && offchainConfig[fileName].hasOwnProperty(contractName) && artifact["address"][fileName][contractName]) {
+                        offchainConfig[fileName][contractName]["address"] = artifact["address"][fileName][contractName];
+                    }
                 }
             }
         }
     }
 
     fs.writeFileSync("results.json", JSON.stringify(results, null, 4));
-    fs.writeFileSync("offchain_config.json", JSON.stringify(offchainConfig, null, 4));
     visualize(results);
     console.log("Results written to results.json");
 
     if (autoStart) {
-        const command = `ityfuzz evm --builder-artifacts-file ./results.json --offchain-config-file ./offchain_config.json -f -i -t "a" --work-dir ./workdir`;
+        let command = "";
+        if (setupFile) {
+            command = `ityfuzz evm --builder-artifacts-file ./results.json -t "a" --work-dir ./workdir --setup-file ${setupFile}`;
+        } else {
+            command = `ityfuzz evm --builder-artifacts-file ./results.json --offchain-config-file ./offchain_config.json -f -i -t "a" --work-dir ./workdir`;
+        }
         console.log(`Starting ityfuzz with command: ${command}`);
         exec(command, (error, stdout, stderr) => {
             if (error) {
@@ -112,7 +122,9 @@ async function build_with_autodetect(project, projectType, compiler_version, dae
         await new Promise(() => { });
     }
 
-    anvil.kill();
+    if (!setupFile) {
+        anvil.kill();
+    }
 }
 
 const argv = yargs(hideBin(process.argv))
@@ -127,7 +139,7 @@ const argv = yargs(hideBin(process.argv))
         },
         async (argv) => {
             if (argv.project) {
-                await build_with_autodetect(argv.project, argv.projectType, argv.compilerVersion, argv.daemon, argv.autoStart);
+                await build_with_autodetect(argv.project, argv.projectType, argv.compilerVersion, argv.daemon, argv.autoStart, argv.setupFile);
             }
         }
     )
@@ -150,6 +162,11 @@ const argv = yargs(hideBin(process.argv))
         alias: 's',
         type: 'boolean',
         description: 'Automatically run ityfuzz after building',
+    })
+    .option('setup-file', {
+        alias: 'f',
+        type: 'string',
+        description: 'Specify the setup file to use',
     })
     .demandOption(['project'], 'Please provide the project argument to proceed')
     .help()
